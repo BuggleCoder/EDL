2ee16a99662f426f788d99336b80fd64e1bbc860
BuggleCoder <pennmcardle32@wrsdk12.net>
2026-09-22 07:10:08 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,27 @@
+import demonList from "./json/list.json" with { type: "json" };
+import info from "./json/info.json" with { type: "json" };
+
+let listBtn = $("#list-btn");
+let packBtn = $("#pack-btn");
+let list = $("#demon-list-container");
+
+function renderList(amount) {
+  list.empty();
+  for (let i = 0; i < amount; i++) {
+    if (!demonList[i]) break;
+    list.append(`
+    <span id="${demonList[i].name}">
+      <li>
+        <p>
+          <strong>#${i + 1}</strong>
+          <br>
+          <strong>${demonList[i].name}</strong> By 
+          <strong>${demonList[i].creator}</strong>
+        </p>
+      </li>
+    </span>
+    `);
+  }
+}
+
+listBtn.on("click", () => renderList(150));
